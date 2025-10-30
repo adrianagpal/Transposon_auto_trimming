bd07f70cfc09eadaa56aaee56e8fc33a2be80a8d
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-10-30 01:42:00 +0100

    Avoid use of gpu

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#SBATCH -o simulation3.out
-#SBATCH -e simulation3.err
+#SBATCH -o generacion_datos.out
+#SBATCH -e generacion_datos.err
 #SBATCH --mail-type END
 #SBATCH --mail-user agonzalezpalo@uoc.edu
-#SBATCH -J simulation_old_dataset
+#SBATCH -J agp_gen_datos
 #SBATCH --time 1-00:00:00
-#SBATCH --partition gpu
-#SBATCH -n 2
+#SBATCH -n 20
 #SBATCH -N 1
-#SBATCH --gres=gpu:1
 #SBATCH --mem 200GB
 
-module load tensorflow-gpu/2.6.2
 source /shared/home/sorozcoarias/anaconda3/bin/activate gpu
 
 cd /shared/home/sorozcoarias/tagua_gen_ec/TransposonDLToolkit/auto_curation_v2/auto_trimming_Adriana
 
-~/anaconda3/envs/gpu/bin/python3 SimulationData.py --fasta pablo_dataset/dataset_peq.fasta
+~/anaconda3/envs/gpu/bin/python3 GenerationData.py --fasta 1000species_dataset/r.1.5_all.fasta --seq_per_case 20 
