b6dbbecbf654ab531720bc47306ee94a69717525
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-11-02 13:59:32 +0100

    Update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -12,11 +12,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem 200GB
 
-#module load tensorflow-gpu/2.6.2
-source /shared/home/sorozcoarias/anaconda3/bin/activate gpu
+module load tensorflow-gpu/2.6.2
+
+source /shared/home/sorozcoarias/anaconda3/bin/activate agp_gpu
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 cd /shared/home/sorozcoarias/tagua_gen_ec/TransposonDLToolkit/auto_curation_v2/auto_trimming_Adriana
 
-~/anaconda3/envs/gpu/bin/python3 NN_trainingV2.py train pablo_dataset/features_data.npy pablo_dataset/labels_data.npy
+~/anaconda3/envs/agp_gpu/bin/python3 NN_trainingV2.py train new_dataset/features_data.npy new_dataset/labels_data.npy
