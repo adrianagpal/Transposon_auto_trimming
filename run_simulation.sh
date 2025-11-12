1a607767dc07e8c5f119ceca36df6cd870012a96
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-11-12 11:29:47 +0100

    Update run_simulation.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#SBATCH -o generacion_datos.out
-#SBATCH -e generacion_datos.err
+#SBATCH -o agp_datos.out
+#SBATCH -e agp_datos.err
 #SBATCH --mail-type END
 #SBATCH --mail-user agonzalezpalo@uoc.edu
-#SBATCH -J agp_gen_datos
+#SBATCH -J agp_datos
 #SBATCH --time 1-00:00:00
-#SBATCH -n 20
+#SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --mem 200GB
 
-source /shared/home/sorozcoarias/anaconda3/bin/activate gpu
+source /shared/home/sorozcoarias/anaconda3/bin/activate agp_gpu
 
 cd /shared/home/sorozcoarias/tagua_gen_ec/TransposonDLToolkit/auto_curation_v2/auto_trimming_Adriana
 
-~/anaconda3/envs/gpu/bin/python3 GenerationData.py --fasta 1000species_dataset/r.1.5_all.fasta --seq_per_case 20 
+~/anaconda3/envs/agp_gpu/bin/python3 GenerationData.py --fasta 1000species_dataset/r.1.5_all.fasta --seq_per_case 20  
+
