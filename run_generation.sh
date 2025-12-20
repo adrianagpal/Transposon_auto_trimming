4d61052bda8d68d2a91a2ef611cae125ad226d5e
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-12-21 00:23:29 +0100

    Update run_generation.sh

--- run_generation.sh
+++ run_generation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -o agp_datos.out
-#SBATCH -e agp_datos.err
+#SBATCH -o data_gen.out
+#SBATCH -e data_gen.err
 #SBATCH --mail-type END
 #SBATCH --mail-user agonzalezpalo@uoc.edu
-#SBATCH -J agp_datos
+#SBATCH -J data_gen
 #SBATCH --time 1-00:00:00
 #SBATCH -n 1
 #SBATCH -N 1
@@ -12,7 +12,5 @@
 
 source /shared/home/sorozcoarias/anaconda3/bin/activate agp_gpu
 
-cd /shared/home/sorozcoarias/tagua_gen_ec/TransposonDLToolkit/auto_curation_v2/auto_trimming_Adriana
-
-~/anaconda3/envs/agp_gpu/bin/python3 GenerationData.py --fasta 1000species_dataset/r.1.5_all.fasta --seq_per_case 20  
+~/anaconda3/envs/agp_gpu/bin/python3 GenerationData.py --fasta r.1.5_all.fasta --seq_per_case 10
 
