7062108c25109a64ca6ff4037616b9c8d2779ea0
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-12-21 00:14:56 +0100

    improving documentation

--- GenerationData.py
+++ GenerationData.py
@@ -4,16 +4,19 @@ import os
 import multiprocessing
 import argparse
 
-# cadena de ADN aleatoria con la longitud deseada
+# Generate a random DNA string of a certain length
 def generate_string(length):
     nucleotides = ['A', 'T', 'C', 'G']
     return ''.join(random.choices(nucleotides, k=length))
 
-# formato FASTA
+# Generate simulated data
 def generate_simulated_data(case, sequences, total_length=15000):
+
+    # Filter sequences by size for case 2
     min_length = 5000
     filtered_sequences = [seq for seq in sequences if len(seq) < min_length]
 
+    # Case1: random DNA + seq1 + random DNA + seq2 + random DNA
     if case == 1:
         seq1_choice = random.choice(sequences)
         min_starting_pos_1 = min(len(seq1_choice.seq), total_length // 2)
@@ -34,6 +37,7 @@ def generate_simulated_data(case, sequences, total_length=15000):
         final_seq = final_seq[:total_length]
         return f">Caso{case}_{seq1_choice.id}_{starting_pos_1}_{len(seq1_choice.seq)}_{seq1_species}\n{final_seq}"
 
+    # Case2: seq1 + random DNA + seq2 + random DNA + seq1
     elif case == 2:
         if len(filtered_sequences) == 0:
             raise ValueError("No hay secuencias con longitud <5000 para generar el caso 2")
@@ -63,6 +67,7 @@ def generate_simulated_data(case, sequences, total_length=15000):
         final_seq = final_seq[:total_length]
         return f">Caso{case}_{seq2_choice.id}_{starting_pos_2}_{len(seq2_choice.seq)}_{seq2_species}\n{final_seq}"
 
+    # Case3: microsatelites + seq + microsatelites
     elif case == 3:
         monomer_length = random.randint(5, 100)
         monomer = generate_string(monomer_length)
@@ -83,29 +88,31 @@ def generate_simulated_data(case, sequences, total_length=15000):
 
         return f">Caso{case}_{random_sequence.id}_{start_pos}_{sequence_length}_{seq_species}\n{microsatelite}"
 
+    # Case4: random DNA + seq + random DNA
     elif case == 4: 
+    
+        seq_choice = random.choice(sequences)        
+        min_starting_pos = min(len(seq_choice.seq), total_length // 2)
+        starting_pos = random.randint(min_starting_pos, total_length // 2)
+        seq_species = " ".join(seq_choice.description.split(" ")[1:]).replace(" ", "_")   
         
-        starting_pos = random.randint(0, 3000)
-        ending_pos = random.randint(0, 3000)       
+        final_seq = generate_string(starting_pos) + str(seq_choice.seq)   
+
+        remaining = total_length - len(final_seq)
         
-        sequence = random.choice(sequences)
-        seq_species = " ".join(sequence.description.split(" ")[1:]).replace(" ", "_")
-        seq4_seq = str(sequence.seq)
-        
-        final_seq = generate_string(starting_pos) + seq4_seq + generate_string(ending_pos)
-        
+        final_seq += generate_string(remaining)
         final_seq = final_seq[:total_length]
 
-        return f">Caso{case}_{sequence.id}_{starting_pos}_{len(seq4_seq)}_{seq_species}\n{seq4_seq}"
+        return f">Caso{case}_{seq_choice.id}_{starting_pos}_{len(seq_choice.seq)}_{seq_species}\n{final_seq}"
 
-# Output
+# Generate simulated data using multiprocessing to execute several processes at the same time
 def generation_multiprocessing(sequences, n, output_file): 
     results = [] 
-    for case in range(1, 5):
-        for _ in range(n): 
+    for case in range(1, 5): # 4 cases
+        for _ in range(n): # n = number of sequences per process  
             results.append(generate_simulated_data(case, sequences)) 
             
-        # write sequentially inside each process to avoid file conflicts 
+        # Write sequentially inside each process to avoid file conflicts 
         with open(output_file, "w") as simulated_data: 
             for r in results: 
                 simulated_data.write(r + "\n")
@@ -114,20 +121,21 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--fasta", required=True, help="Archivo FASTA de entrada")
-    parser.add_argument("--seq_per_case", type=int, default=1000, help="Número de secuencias por caso a generar ")
-    parser.add_argument("--processes", type=int, default=20, help="Número de procesos por nodo")
+    parser.add_argument("--seq_per_case", type=int, default=1000, help="Numero de secuencias por caso a generar")
+    parser.add_argument("--processes", type=int, default=20, help="Numero de procesos por nodo")
     args = parser.parse_args()
 
     if not os.path.isfile(args.fasta):
-        raise FileNotFoundError(f"No se encontró {args.fasta}")
+        raise FileNotFoundError(f"No se encontro {args.fasta}")
 
+    # Save sequences in a list of SeqRecord objects
     with open(args.fasta, "r") as fasta_file:
         sequences = list(SeqIO.parse(fasta_file, "fasta"))
 
     if not sequences:
         raise ValueError("No se encontraron secuencias en el FASTA")
 
-    # Dividir el trabajo entre procesos
+    # Divide sequences to generate by each process
     total_to_generate = args.seq_per_case
     seqs_per_process = total_to_generate // args.processes
     processes = []
@@ -150,11 +158,12 @@ if __name__ == "__main__":
     for p in processes: 
         p.join()
 
+    # Join temporary files 
     with open("simulated_data_merged.fasta", "w") as outfile:
         for i in range(args.processes):
             part_file = f"simulated_data_part{i + 1}.fasta"
             with open(part_file, "r") as infile:
                 outfile.write(infile.read())
-            os.remove(part_file)  # elimina los archivos temporales
+            os.remove(part_file)  # removes temporary files
 
-print("✅ Secuencias generadas correctamente.")
+print("Secuencias generadas correctamente.")
