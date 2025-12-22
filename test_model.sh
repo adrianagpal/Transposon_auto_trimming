6e4a3e86d127d133c6b78124e9ecec5d28da272b
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2025-12-22 23:08:11 +0100

    Create test_model.sh

---
+++ test_model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -o test_model.out
+#SBATCH -e test_model.err
+#SBATCH --mail-type END
+#SBATCH --mail-user agonzalezpalo@uoc.edu
+#SBATCH -J test_model
+#SBATCH --partition long
+#SBATCH --time 1-00:00:00
+#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH --mem 200GB
+
+module load tensorflow-gpu/2.6.2
+
+export TF_GPU_ALLOCATOR=cuda_malloc_async
+
+source /shared/home/sorozcoarias/anaconda3/bin/activate training_agp
+
+/shared/ifbstor1/projects/tagua_gen_ec/anaconda3_homesimon/envs/training_agp/bin/python3 test_model.py ./models/new_VGG16_BN/trained_model.h5 ./models/new_VGG16_BN/scalerX.bin ./dataset_pred_grande
