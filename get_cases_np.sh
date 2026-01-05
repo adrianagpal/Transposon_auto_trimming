19b3392c5e2118d045fa58c2439752751d68bd32
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2026-01-05 12:31:25 +0100

    Create get_cases_np.sh

---
+++ get_cases_np.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -o agp_species.out
+#SBATCH -e agp_species.err
+#SBATCH --mail-type END
+#SBATCH --mail-user agonzalezpalo@uoc.edu
+#SBATCH -J agp_species
+#SBATCH --time 1-00:00:00
+#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH --mem 200GB
+
+source /shared/home/sorozcoarias/anaconda3/bin/activate auto_trimming_agp
+
+~/anaconda3/envs/auto_trimming_agp/bin/python3 get_cases_np.py 
