24f8af82979e53bef31dd9aa0db7bed5e918eeca
adrianagpal <144604178+adrianagpal@users.noreply.github.com>
2026-01-05 12:28:50 +0100

    Update run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-#SBATCH -o training.out
-#SBATCH -e training.err
+#SBATCH -o resnet18_30000.out
+#SBATCH -e resnet18_30000.err
 #SBATCH --mail-type END
 #SBATCH --mail-user agonzalezpalo@uoc.edu
-#SBATCH -J training
+#SBATCH -J resnet18_30000
 #SBATCH --time 1-00:00:00
-#SBATCH --partition gpu
-#SBATCH -n 2
+#SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
-#SBATCH --mem 200GB
+#SBATCH --mem 400GB
 
 module load tensorflow-gpu/2.6.2
 
-source /shared/home/sorozcoarias/anaconda3/bin/activate agp_gpu
+source /shared/home/sorozcoarias/anaconda3/bin/activate auto_trimming_agp
 
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-cd /shared/home/sorozcoarias/tagua_gen_ec/TransposonDLToolkit/auto_curation_v2/auto_trimming_Adriana
-
-~/anaconda3/envs/agp_gpu/bin/python3 NN_trainingV2.py train new_dataset/features_data.npy new_dataset/labels_data.npy
+~/anaconda3/envs/training_agp/bin/python3 resnet18_30000.py test trained_model.h5 scalerX.bin X_test.npy Y_test.npy
